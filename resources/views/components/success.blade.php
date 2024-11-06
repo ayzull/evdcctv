b8242ce60d2593031a0ded8905c7b8ada630de2a
ayzul <aizulismaad@gmail.com>
2024-11-06 14:18:29 +0800

    path

--- resources/views/components/success.blade.php
+++ resources/views/components/success.blade.php
@@ -1,6 +1,6 @@
 {{-- resources\views\components\success.blade.php --}}
-if (session('success'))
-<div class="bg-green-100 text-green-700 p-4 rounded mb-6">
-    {{ session('success') }}
-</div>
+@if (session('success'))
+    <div class="bg-green-100 text-green-700 p-4 rounded mb-6">
+        {{ session('success') }}
+    </div>
 @endif
